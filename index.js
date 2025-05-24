03fb87d788ba4b68aca6a40dcef70dbe94acc821
arifurrahmansajid <iamnobody3456@gmail.com>
2025-05-24 22:31:56 +0600

    uplode url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: [ "http://localhost:5173",], 
+    origin: [ "http://localhost:5173", "https://plant-website-a0f51.web.app/"], 
     credentials: true,
   })
 );
